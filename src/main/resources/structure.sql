b4d6b2b84fcb37718fd5a619a55a4f4fca977706
Meldanor <Bhaals@gmx.de>
2012-05-16 22:42:50 +0200

    Clean up the project Add some standards like substitute the version in the config and depending on minestarlibrary Add launch file for maven build
    Signed-off-by: Meldanor <Bhaals@gmx.de>

--- src/main/resources/structure.sql
+++
@@ -1,9 +0,0 @@
-CREATE  TABLE IF NOT EXISTS `stats` (
-	`id` INT NOT NULL AUTO_INCREMENT ,
-    `player` VARCHAR(45) NOT NULL ,
-    `loginGroup` INT NOT NULL ,
-    loginTime` DATETIME NOT NULL ,
-    `logoutGroup` INT ,
-    `logoutTime` DATETIME ,
-    PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
